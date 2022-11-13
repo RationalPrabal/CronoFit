af8e25e65b3794c0d1ca840bc2d87d19bef50a81
RationalPrabal <prabal19999@gmail.com>
2022-11-13 19:23:35 +0530

    final correction

--- blog.js
+++ blog.js
@@ -1,16 +1,12 @@
 // importing navbar
 
-import { navbar } from "./nav-components/navbar.js";
+import { navbar } from "./navbar.js";
 
 let nav= document.getElementById("navbar");
 nav.innerHTML=navbar();
 
 //importing footer
 
-// import { footer } from "./footer/footer.js";
-
-// let footer_div= document.getElementById("footer_div");
-// footer_div.innerHTML= footer();
 
 
 
@@ -19,4 +15,4 @@ const directToHome=()=>{
 window.location.href="index.html"
 }
 
-document.querySelector("#navbar>:nth-child(1)>img").addEventListener("click", directToHome);
+document.querySelector(".site-logo").addEventListener("click", directToHome);
